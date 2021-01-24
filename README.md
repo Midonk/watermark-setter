695adb0328391d815094b9dcf4d09ffbc8a66444
DESKTOP-SRL43AG\Midonk <t_finet@hotmail.fr>
2021-01-24 19:34:13 +0100

    v1.0

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # watermark-setter
-This Adobe Photoshop script is used to set a watermark at a corner of any images opened in the soft.
+version 1.0
+Photoshop version : 21.x (and surely below)
+
+This Adobe Photoshop script is used to set a watermark at a corner of all opened images.
 In addition, you can specify an offset for your watermark / signature or anything you want.
 You will also be able to choose the layer you want to use as the watermark.
 
 ## Interface
-The script has a pretty simple use interface. Quite handy when you need more flexibility.
-- Offset settings: Determine the X and Y value for the offfset of your watermark in the corner. Yout have the possibility to link both values
-- Corner selection: Determine in wich corner you want to set your watermark
+The script has a pretty simple user interface. Quite handy when you need more flexibility.
+- Offset settings: Determine the X and Y value for the offset of your watermark in the corner. You have the possibility to link both values
+*Good to know: the offset is computed depending the selected corner. You may prefer ask yourself "from how many pixels of the corner do I want to set my watermark"*
+*Example: the top-right corner is selected, then [x, y] offset will translate the watermark to the [left, down] by default*
+- Corner selection: Determine in which corner you want to set your watermark
 - Layer dropdown: Used to select the layer to use as watermark
--- watermark-setter.jsx
+++ watermark-setter.jsx
@@ -12,84 +12,110 @@ var facteurH = 1;
 //vertical factor used to calculate the offset and the watermark placement
 var facteurV = 1;
 //Interface window
-var win;
-var progressBar;
+var win = null;
+//progress bar window
+var progressBar = null;
+//opened documents list
+var openedDocuments = app.documents;
+
+function setup(){
+    //no document opened
+    if(openedDocuments.length < 1){
+        alert("You need to have at least one open document.", title="Error");
+        return false;
+    }
+
+    //less than 2 layer => unable to run
+    if(app.activeDocument.layers.length < 2){
+        alert("Unable to run the script: you need to have a watermark\nand another layer on the active document", title="Error");
+        return false;
+    }
+
+    else return true;
+}
 
 //Create UI to setup the script
 function createUI(){
     win = new Window("dialog", "Watermark setter");
     win.orientation = "column";
 
+    //____________________________________________________________________
     //Create the border spacing panel
     win.offsetPanel = win.add("panel", undefined, "Border offset");
     win.offsetPanel.alignment = "fill";
 
-    //Horizontal offset
-    win.offsetPanel.hGrp = win.offsetPanel.add("group", undefined);
-    win.offsetPanel.hGrp.orientation = "row";
+        //Horizontal offset
+        win.offsetPanel.hGrp = win.offsetPanel.add("group", undefined);
+        win.offsetPanel.hGrp.orientation = "row";
 
-    win.offsetPanel.hGrp.add("statictext", undefined, "H");
-    win.offsetPanel.hGrp.hOffsetValue = win.offsetPanel.hGrp.add("edittext", undefined);
-    win.offsetPanel.hGrp.hOffsetValue.text = 0;
-    win.offsetPanel.hGrp.hOffsetValue.addEventListener("change", function(evt){setHValue(evt, hValue)});
-    win.offsetPanel.hGrp.add("statictext", undefined, "px");
+            win.offsetPanel.hGrp.add("statictext", undefined, "H");
+            win.offsetPanel.hGrp.hOffsetValue = win.offsetPanel.hGrp.add("edittext", undefined);
+            win.offsetPanel.hGrp.hOffsetValue.text = 0;
+            win.offsetPanel.hGrp.hOffsetValue.addEventListener("change", function(evt){setHValue(evt, hValue)});
+            win.offsetPanel.hGrp.add("statictext", undefined, "px");
 
-    //Vertical offset
-    win.offsetPanel.vGrp = win.offsetPanel.add("group", undefined);
-    win.offsetPanel.vGrp.orientation = "row";
+        //Vertical offset
+        win.offsetPanel.vGrp = win.offsetPanel.add("group", undefined);
+        win.offsetPanel.vGrp.orientation = "row";
 
-    win.offsetPanel.vGrp.add("statictext", undefined, "V");
-    win.offsetPanel.vGrp.vOffsetValue = win.offsetPanel.vGrp.add("edittext", undefined);
-    win.offsetPanel.vGrp.vOffsetValue.text = 0;
-    win.offsetPanel.vGrp.vOffsetValue.addEventListener("change", function(evt){setHValue(evt, vValue)});
-    win.offsetPanel.vGrp.add("statictext", undefined, "px");
+            win.offsetPanel.vGrp.add("statictext", undefined, "V");
+            win.offsetPanel.vGrp.vOffsetValue = win.offsetPanel.vGrp.add("edittext", undefined);
+            win.offsetPanel.vGrp.vOffsetValue.text = 0;
+            win.offsetPanel.vGrp.vOffsetValue.addEventListener("change", function(evt){setHValue(evt, vValue)});
+            win.offsetPanel.vGrp.add("statictext", undefined, "px");
 
-    //Link both offsets values or not
-    win.offsetPanel.linkCheck = win.offsetPanel.add("checkbox", undefined, "Keep linked");
-    win.offsetPanel.linkCheck.addEventListener("click", switchLink);
+        //Link both offsets values or not
+        win.offsetPanel.linkCheck = win.offsetPanel.add("checkbox", undefined, "Keep linked");
+        win.offsetPanel.linkCheck.addEventListener("click", switchLink);
 
+    //____________________________________________________________________
     //Create the localisation panel for the watermark
-    win.markLoc = win.add("panel", undefined, "Watermark localisation");
+    win.markLoc = win.add("panel", undefined, "Watermark corner");
     win.markLoc.orientation = 'column';
-    win.markLoc.alignment = "fill";
+    //win.markLoc.alignment = "fill";
 
-    win.markLoc.radioGrp = win.markLoc.add("group", undefined);
-    win.markLoc.radioGrp.orientation = 'column';
+        win.markLoc.radioGrp = win.markLoc.add("group", [0, 0, 190, 100]);
+        //win.markLoc.radioGrp.orientation = "column";
 
-    win.markLoc.radioGrp.alignTopLeft = win.markLoc.radioGrp.add("radiobutton", undefined, "TL");
-    win.markLoc.radioGrp.alignTopLeft.alignement = ["left", "top"];
-    win.markLoc.radioGrp.alignTopLeft.tag = "TL";
-    win.markLoc.radioGrp.alignTopLeft.addEventListener("click", switchCorner);
+            win.markLoc.radioGrp.alignTopLeft = win.markLoc.radioGrp.add("radiobutton", [0, 0, 15, 15], "");
+            //win.markLoc.radioGrp.alignTopLeft.alignment = "left";
+            win.markLoc.radioGrp.alignTopLeft.tag = "TL";
+            win.markLoc.radioGrp.alignTopLeft.addEventListener("click", switchCorner);
 
-    win.markLoc.radioGrp.alignTopRight = win.markLoc.radioGrp.add("radiobutton", undefined, "TR");
-    win.markLoc.radioGrp.alignTopRight.alignement = ["right", "top"];
-    win.markLoc.radioGrp.alignTopRight.tag = "TR";
-    win.markLoc.radioGrp.alignTopRight.addEventListener("click", switchCorner);
+            win.markLoc.radioGrp.alignTopRight = win.markLoc.radioGrp.add("radiobutton", [175, 0, 15, 15], "");
+            //win.markLoc.radioGrp.alignTopRight.locatioalignmentn = "right"
+            win.markLoc.radioGrp.alignTopRight.tag = "TR";
+            win.markLoc.radioGrp.alignTopRight.addEventListener("click", switchCorner);
 
-    win.markLoc.radioGrp.alignBottomLeft = win.markLoc.radioGrp.add("radiobutton", undefined, "BL");
-    win.markLoc.radioGrp.alignBottomLeft.alignement = ["left", "bottom"];
-    win.markLoc.radioGrp.alignBottomLeft.tag = "BL";
-    win.markLoc.radioGrp.alignBottomLeft.addEventListener("click", switchCorner);
+            win.markLoc.radioGrp.alignBottomLeft = win.markLoc.radioGrp.add("radiobutton", [0, 85, 15, 15], "");
+            //win.markLoc.radioGrp.alignBottomLeft.alignment = "left";
+            win.markLoc.radioGrp.alignBottomLeft.tag = "BL";
+            win.markLoc.radioGrp.alignBottomLeft.addEventListener("click", switchCorner);
 
-    win.markLoc.radioGrp.alignBottomRight = win.markLoc.radioGrp.add("radiobutton", undefined, "BR");
-    win.markLoc.radioGrp.alignBottomRight.alignement = ["right", "bottom"];
-    win.markLoc.radioGrp.alignBottomRight.tag = "BR";
-    win.markLoc.radioGrp.alignBottomRight.addEventListener("click", switchCorner);
-    win.markLoc.radioGrp.alignBottomRight.value = true;
+            win.markLoc.radioGrp.alignBottomRight = win.markLoc.radioGrp.add("radiobutton", [175, 85, 15, 15], "");
+            //win.markLoc.radioGrp.alignBottomRight.alignment = "right";
+            win.markLoc.radioGrp.alignBottomRight.tag = "BR";
+            win.markLoc.radioGrp.alignBottomRight.addEventListener("click", switchCorner);
+            win.markLoc.radioGrp.alignBottomRight.value = true;
 
+    //____________________________________________________________________
     //Create a dropdown list to select the layer to use as
-    win.dropList = win.add("DropDownList", undefined, setListItems());
+    win.dropList = win.add("DropDownList", undefined, getLayerList());
     win.dropList.title = "Watermark  selection";
     win.dropList.selection = win.dropList.items[0];
     win.dropList.alignment = "fill";
 
+    //____________________________________________________________________
     win.confirmBtnGrp = win.add("group", undefined);
-    win.confirmBtnGrp.alignment = "fill";
+    //win.confirmBtnGrp.alignment = "fill";
 
-    win.confirmBtnGrp.okBtn = win.confirmBtnGrp.add("button", undefined, "Ok");
-    win.confirmBtnGrp.okBtn.onClick = WaterMarkSetter;
+        win.confirmBtnGrp.okBtn = win.confirmBtnGrp.add("button", undefined, "Ok", {name: "ok"});
+        win.confirmBtnGrp.okBtn.onClick = WaterMarkSetter;
+        
+        win.confirmBtnGrp.okBtn = win.confirmBtnGrp.add("button", undefined, "Apply to all");
+        win.confirmBtnGrp.okBtn.onClick = WaterMarkSetterAll;
 
-    win.confirmBtnGrp.cancelBtn = win.confirmBtnGrp.add("button", undefined, "Cancel", {name: "cancel"});
+        win.confirmBtnGrp.cancelBtn = win.confirmBtnGrp.add("button", undefined, "Cancel", {name: "cancel"});
 
     win.show();
 }
@@ -98,11 +124,11 @@ function createProgressBar(){
     var pgb = new Window("palette", "Processing...");
 
     pgb.bar = pgb.add("progressbar", undefined);
-    pgb.bar.value = 1;
-
-    progressBar = pgb;
+    pgb.bar.value = 0;
 
     pgb.show();
+
+    progressBar = pgb;
 }
 
 //Test if the link between both value is made or not and set up the interface following it
@@ -136,6 +162,7 @@ function switchLink(evt) {
     }
 }
 
+//set factor signs when the corner selection change
 function switchCorner(evt) {
     switch (evt.currentTarget.tag) {
         case "BR":
@@ -161,8 +188,9 @@ function switchCorner(evt) {
 }
 
 //creating items for dropDownList to choose watermark
-function setListItems() {
+function getLayerList() {
     var items = [];
+
     for (var i = 0; i < app.activeDocument.layers.length; i++) {
         items.push(app.activeDocument.layers[i].name);
     }
@@ -173,45 +201,61 @@ function setListItems() {
 //Initialize variable needed to the script
 function Initialization() {
     waterMarkLayer = app.activeDocument.layers.getByName(win.dropList.selection);
-    waterMarkLayer.copy();
-    waterMarkLayer.remove();
-    progressBar.bar.value += 5;
+    vValue = parseInt(win.offsetPanel.vGrp.vOffsetValue.text, 10);
+    hValue = parseInt(win.offsetPanel.hGrp.hOffsetValue.text, 10);
+    waterMarkLayer.cut();
 }
 
+//Set watermark for active document
 function WaterMarkSetter() {
-    var translateX;
-    var translateY;
-
-    createProgressBar();
     Initialization();
-
-    for (var i = 0; i < app.documents.length; i++) {
-        var currentDoc = app.documents[i];
-        app.activeDocument = currentDoc;
-
-        var copiedLayer = currentDoc.paste();
-        copiedLayer.name = "Watermark";
-        //calculate the width and height of the copiedLayer
-        //[0, 1, 2, 3] => [Ax, Ay, Bx, By] => [TLx, TLy, BRx, BRy]
-        var clWidth = copiedLayer.bounds[2] - copiedLayer.bounds[0];
-        var clHeight = copiedLayer.bounds[3] - copiedLayer.bounds[1];
-        //met le coin haut gauche au corner sélectionné
-        translateX = (currentDoc.width - copiedLayer.bounds[0]) * facteurH;
-        translateY = (currentDoc.height - copiedLayer.bounds[1]) * facteurV;
-        //positionning correctly watermark in workspace
-        translateX -= clWidth * facteurH;
-        translateY -= clHeight * facteurV;
-        //applying offset
-        translateX -= hValue * facteurH;
-        translateY -= vValue * facteurV;
-
-        copiedLayer.translate(translateX, translateY);
-
-        progressBar.bar.value += (100 - 5) / app.documents.length;
-    }
-
-    progressBar.close();
+    app.activeDocument.suspendHistory("Watermark setter", "process()");
     win.close();
 }
 
+//Set a watermark for all opened documents
+function WaterMarkSetterAll() {
+    createProgressBar();
+    Initialization();
+
+    for (var i = 0; i < openedDocuments.length; i++) {
+        var currentDoc = openedDocuments[i];
+        app.activeDocument = currentDoc;
+
+        currentDoc.suspendHistory("Watermark setter", "process()");
+
+        progressBar.bar.value += (100 / openedDocuments.length - 1) * i;
+        progressBar.show();
+    }
+
+    win.close();
+    progressBar.close();
+}
+
+//main process of setting the watermark to the selected corner
+function process(){
+    var translateX;
+    var translateY;
+    var currentDoc = app.activeDocument
+    var copiedLayer = currentDoc.paste();
+    copiedLayer.name = "Watermark";
+    //calculate the width and height of the copiedLayer
+    //[0, 1, 2, 3] => [Ax, Ay, Bx, By] => [TLx, TLy, BRx, BRy]
+    var clWidth = copiedLayer.bounds[2] - copiedLayer.bounds[0];
+    var clHeight = copiedLayer.bounds[3] - copiedLayer.bounds[1];
+    //set the top left corner of the watermark on the selected corner
+    translateX = (currentDoc.width - copiedLayer.bounds[0]) * facteurH;
+    translateY = (currentDoc.height - copiedLayer.bounds[1]) * facteurV;
+    //positionning correctly watermark in workspace
+    translateX -= clWidth * facteurH;
+    translateY -= clHeight * facteurV;
+    //applying offset
+    translateX -= hValue * facteurH;
+    alert(hValue*facteurH);
+    translateY -= vValue * facteurV;
+
+    copiedLayer.translate(translateX, translateY);
+}
+
+if(setup())
 createUI();
